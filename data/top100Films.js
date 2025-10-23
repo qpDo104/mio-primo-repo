8245c9c4112eaaf5614d1d28d351ea93ae71cbf9
ciccio <fraplatania95@gmail.com>
2025-10-23 16:02:41 +0200

    primo commit

---
+++ data/top100Films.js
@@ -0,0 +1,9 @@
+
+const top100Films = [
+  { label : "Marco Galluzzo, as STALLION"},
+  { label : "Dario Ferracci, as MOONRYDE"},
+{ label : "Megan Garner, as MEGSOUNDSLIKEEGG"},
+  { label : "Ester Bjøru, as EASTAHH"},
+];
+
+export default top100Films;
