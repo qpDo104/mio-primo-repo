3a67666c9fdf36afb1fe350c505d037d7d51b98d
ciccio <fraplatania95@gmail.com>
2025-10-27 15:33:14 +0100

    speriamo mi metta il next.js

--- app/data/moonryde/page.tsx
+++ app/data/moonryde/page.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 export default function SplitButton() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <div className="scritta3"><a href="https://www.youtube.com/watch?v=oCMbdqDfbzI"><img src="/images.jpg" height="200"></img>video più popolari</a></div>
-        <div className="scritta4"><a href="https://www.youtube.com/watch?v=hYo9xUe0GJ0"><img src="/download.jpg" height="200"></img>video più popolari</a></div>
-        <div className="scritta5"><a href="https://www.youtube.com/watch?v=I2k68B4oZcU"><img src="/m4.jpg"></img>video più popolari</a></div>
+        <div className="scritta3"><a href="https://www.youtube.com/watch?v=t-p4Lj7UuTA"><img src="/cech.png" height="200"></img>video più popolari</a></div>
+        <div className="scritta4"><a href="https://www.youtube.com/watch?v=ir5c8FZ24N8"><img src="/1000.png" height="200"></img>video più popolari</a></div>
+        <div className="scritta5"><a href="https://www.youtube.com/watch?v=6OcBjo30Tv4"><img src="/duo.png"></img>video più popolari</a></div>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>
         <Image 
-          src="/stallion.png"
+          src="/moon.jpg"
           alt="Next.js logo"
           width={2500}
           height={200}
@@ -20,10 +20,14 @@ export default function SplitButton() {
         />
        
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-            <p className="scritta1"><p className="scritta">Stallion</p> è un creatore di contenuti, streamer ed ex conduttore freelance con sede nel Regno Unito,
-             ora sotto contratto con l'organizzazione di eSport Fnatic.
-               <p className="scritta">noto per lo streaming di FPS e Call Of Duty MW</p>per aver costruito la sua community e per aver fornito consulenza sulla creazione di contenuti. 
+            <p className="scritta1"><p className="scritta">Moonryde</p> Dario “Moonryde” Ferracci è uno dei content creator più seguiti sulla piattaforma viola, ma anche <p className="scritta">uno dei migliori giocatori professionisti di Call of Duty: Warzone</p>
+             Da sempre affronta il mondo videoludico con costanza,
+             serietà e competitività, grazie a un mind-set costruito attraverso una carriera nel basket.
+             La sua passione per i videogiochi arriva da lontano, con Overboard per PS1 – il suo primissimo videogioco –, poi God of War, per poi sbarcare su Twitch nel 2015 con League of Legends,
+             uno dei titoli con cui ha cominciato la sua attività sulla piattaforma streaming.
                </p>
+               <p>{"‎ "}</p>
+          <p>{"‎ "} </p>
                </div>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
